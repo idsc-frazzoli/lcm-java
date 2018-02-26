78750d2bbc047ad588b3e2252d29eb9f4de87f0b
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-02-26 12:26:20 +0100

    code format

--- src/main/java/lcm/logging/BufferedZipInputStream.java
+++ src/main/java/lcm/logging/BufferedZipInputStream.java
@@ -6,96 +6,90 @@ import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import lcm.logging.Log;
 import lcm.util.BufferedRandomAccessFile;
 
-/**
- * Class for reading compressed input stream sequentially. Similar to
+/** Class for reading compressed input stream sequentially. Similar to
  * {@link BufferedRandomAccessFile} for {@link Log}, but for
  * {@link ZipLogEventReader}.
  * 
- * @author anritter
- *
- */
+ * @author anritter */
 public class BufferedZipInputStream {
+  private ZipInputStream zis;
 
-    private ZipInputStream zis;
+  public BufferedZipInputStream(String path) throws IOException {
+    zis = new ZipInputStream(new FileInputStream(path));
+    ZipEntry entry = null;
+    while ((entry = zis.getNextEntry()) != null)
+      // entry name must be equal to file name of path
+      if (path.endsWith(entry.getName() + ".zip"))
+        break;
+  }
 
-    public BufferedZipInputStream(String path) throws IOException {
-        zis = new ZipInputStream(new FileInputStream(path));
-        ZipEntry entry = null;
-        while ((entry = zis.getNextEntry()) != null)
-            // entry name must be equal to file name of path
-            if (path.endsWith(entry.getName() + ".zip"))
-                break;
-    }
+  public void close() throws IOException {
+    zis.close();
+  }
 
-    public void close() throws IOException {
-        zis.close();
-    }
+  public final int read() throws IOException {
+    int byteRead = zis.read();
+    if (byteRead == -1)
+      throw new EOFException("EOF");
+    return byteRead & 0xff;
+  }
 
-    public final int read() throws IOException {
-        int byteRead = zis.read();
-        if (byteRead == -1)
-            throw new EOFException("EOF");
-        return byteRead & 0xff;
-    }
+  public boolean readBoolean() throws IOException {
+    return read() != 0;
+  }
 
-    public boolean readBoolean() throws IOException {
-        return read() != 0;
-    }
+  public byte readByte() throws IOException {
+    return (byte) read();
+  }
 
-    public byte readByte() throws IOException {
-        return (byte) read();
-    }
+  public short readShort() throws IOException {
+    short v = 0;
+    v |= (read() << 8);
+    v |= (read());
+    return v;
+  }
 
-    public short readShort() throws IOException {
-        short v = 0;
-        v |= (read() << 8);
-        v |= (read());
-        return v;
-    }
+  public void readFully(byte[] b, int offset, int length) throws IOException {
+    int numRead = zis.read(b, offset, length);
+    if (numRead == -1)
+      throw new EOFException("EOF");
+    if (numRead != length) // happens. Read again remaining bytes.
+      readFully(b, offset + numRead, length - numRead);
+  }
 
-    public void readFully(byte[] b, int offset, int length) throws IOException {
-        int numRead = zis.read(b, offset, length);
-        if (numRead == -1)
-            throw new EOFException("EOF");
-        if (numRead != length) // happens. Read again remaining bytes.
-            readFully(b, offset + numRead, length - numRead);
-    }
+  public void readFully(byte[] b) throws IOException {
+    readFully(b, 0, b.length);
+  }
 
-    public void readFully(byte[] b) throws IOException {
-        readFully(b, 0, b.length);
-    }
+  public int readInt() throws IOException {
+    int v = 0;
+    v |= (read() << 24);
+    v |= (read() << 16);
+    v |= (read() << 8);
+    v |= (read());
+    return v;
+  }
 
-    public int readInt() throws IOException {
-        int v = 0;
-        v |= (read() << 24);
-        v |= (read() << 16);
-        v |= (read() << 8);
-        v |= (read());
-        return v;
-    }
+  public long readLong() throws IOException {
+    long v = 0;
+    v |= (((long) read()) << 56);
+    v |= (((long) read()) << 48);
+    v |= (((long) read()) << 40);
+    v |= (((long) read()) << 32);
+    v |= (((long) read()) << 24);
+    v |= (((long) read()) << 16);
+    v |= (((long) read()) << 8);
+    v |= read();
+    return v;
+  }
 
-    public long readLong() throws IOException {
-        long v = 0;
-        v |= (((long) read()) << 56);
-        v |= (((long) read()) << 48);
-        v |= (((long) read()) << 40);
-        v |= (((long) read()) << 32);
-        v |= (((long) read()) << 24);
-        v |= (((long) read()) << 16);
-        v |= (((long) read()) << 8);
-        v |= read();
-        return v;
-    }
+  public float readFloat() throws IOException {
+    return Float.intBitsToFloat(readInt());
+  }
 
-    public float readFloat() throws IOException {
-        return Float.intBitsToFloat(readInt());
-    }
-
-    public double readDouble() throws IOException {
-        return Double.longBitsToDouble(readLong());
-    }
-
+  public double readDouble() throws IOException {
+    return Double.longBitsToDouble(readLong());
+  }
 }
